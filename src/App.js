afde3f9f845032e136e4f0dcf71e5977a40fac2e
viggibot0 <93134590+viggibot0@users.noreply.github.com>
2021-11-02 18:03:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import React from "react";
+import Banner from './components/banner';
+import Navbar from './components/navbar';
+import Editorial from "./components/Editorial/Editorial"
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Banner/>
+      <Editorial/>
+    </div>
+  );
+}
+
+export default App;
